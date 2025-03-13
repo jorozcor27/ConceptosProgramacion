6966321a13583691c142fe257d2856715a682530
jorozcor27 <johanorozcoroa@gmail.com>
2025-03-13 17:00:33 -0500

    ejercicio 1

---
+++ ConceptosProgramacion/ParIMpar/Program.cs
@@ -0,0 +1,17 @@
+﻿// Construir un programa que pida un número y luego diga si este número es par o no.
+
+
+do
+{
+    Console.Write("INGRESAR NUMERO:");
+    var numeroString = Console.ReadLine();
+    var numeroInt = int.Parse(numeroString!);
+    if (numeroInt % 2 == 0)
+    {
+        Console.WriteLine("el numero es par");
+    }
+    else
+    {
+        Console.WriteLine("el numero es impar");
+    }
+}while (true);
